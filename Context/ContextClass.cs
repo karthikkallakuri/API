e862dc07e467d5617976844a7a3b30e6ec5710d8
karthik <karthikkal55@gmail.com>
2020-10-07 15:59:10 -0400

    Todo Items list and Inmemory Db

---
+++ Context/ContextClass.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Context
+{
+    public class ContextClass : DbContext
+    {
+        public ContextClass(DbContextOptions<ContextClass> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
